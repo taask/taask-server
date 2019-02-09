0b6867cff33736a0b2373da371426b89fde46030
cohix <connorjhicks@gmail.com>
2019-02-08 20:09:48 -0500

    add partner service

---
+++ service/partner_service.go
@@ -0,0 +1,82 @@
+package service
+
+import (
+	"fmt"
+	"net"
+
+	log "github.com/cohix/simplog"
+	"github.com/pkg/errors"
+	"github.com/taask/taask-server/auth"
+	"github.com/taask/taask-server/brain"
+	"github.com/taask/taask-server/model"
+	context "golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+const partnerServicePort = ":3690"
+
+// StartPartnerService starts the partner service
+func StartPartnerService(brain *brain.Manager, errChan chan error) {
+	lis, err := net.Listen("tcp", partnerServicePort)
+	if err != nil {
+		errChan <- err
+		return
+	}
+
+	grpcServer := grpc.NewServer()
+
+	RegisterPartnerServiceServer(grpcServer, &PartnerService{Manager: brain})
+
+	log.LogInfo("starting taask-server partner service on :3690")
+	if err := grpcServer.Serve(lis); err != nil {
+		errChan <- err
+	}
+}
+
+// PartnerService describes the service available to taask managing partners
+type PartnerService struct {
+	Manager *brain.Manager
+}
+
+// AuthPartner allows a partner to authenticate and get a session
+func (ps *PartnerService) AuthPartner(ctx context.Context, req *AuthMemberRequest) (*AuthMemberResponse, error) {
+	defer log.LogTrace("AuthPartner")()
+
+	attempt := &auth.Attempt{
+		MemberUUID:  req.UUID,
+		GroupUUID:   auth.DefaultGroupUUID,
+		PubKey:      req.PubKey,
+		AuthHashSig: req.AuthHashSignature,
+		Timestamp:   req.Timestamp,
+	}
+
+	encPartnerChallenge, err := ps.Manager.AttemptRunnerAuth(attempt)
+	if err != nil {
+		return nil, err
+	}
+
+	resp := &AuthMemberResponse{
+		EncChallenge: encPartnerChallenge.EncSessionChallenge,
+		MasterPubKey: ps.Manager.GetMasterRunnerPubKey(),
+	}
+
+	return resp, nil
+}
+
+// StreamUpdates enables sync between partners
+func (ps *PartnerService) StreamUpdates(stream PartnerService_StreamUpdatesServer) error {
+	return nil
+}
+
+func (ps *PartnerService) checkTaskVersion(update *model.TaskUpdate) error {
+	task, err := ps.Manager.GetTask(update.UUID)
+	if err != nil {
+		return errors.Wrap(err, "failed to storage.Get")
+	}
+
+	if update.Version != task.Meta.Version+1 {
+		return fmt.Errorf("runner tried to apply update with version %d to task %s with version %d", update.Version, task.UUID, task.Meta.Version)
+	}
+
+	return nil
+}
